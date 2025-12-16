18b5d34e7bc527e0489c0e8f7e80c3c8a853f8cb
elise-m1 <elise.mensch@ensae.fr>
2025-12-16 19:28:17 +0000

    Création d'une fonction permettant de simplifier les différents statuts des formations

--- function.py
+++ function.py
@@ -1,2 +1,15 @@
 # Il s'agit du fichier dans lequel nous pouvons écrire toutes nos fonctions que nous appelerons dans le notebook
 
+#Importation des librairies qui vont nous servir
+import numpy as np
+import pandas as pd
+
+def simplification_statut(val):
+    """Transforme le statut détaillé en Privé/Public/Autre"""
+    statut = str(val).lower() #robustesse
+    if public in statut :
+        return "Public"
+    elif privé in statut :
+        return "Privé"
+    else :
+        return "Autre"
